d390db078969a98f6d9822584fe774fc4724b09f
Yarlley Fernanes <yarlleyfernandes9@gmail.com>
2023-12-02 10:23:05 -0300

    Seção 18: Arrays e Objetos

--- JavaScript completo/5_arrays_e_objetos/js/script.js
+++ JavaScript completo/5_arrays_e_objetos/js/script.js
@@ -129,3 +129,90 @@ const users = ["Yarlley", "Matheus", "João", "Laura"]
 for(let i = 0; i < users.length; i++) {
     console.log(`Listando o usúario: ${users[i]}`)
 }
+
+// 11 - push e pop
+const array = ["a", "b", "c"]
+
+array.push("d")
+
+console.log(array)
+
+console.log(array.length)
+
+array.pop()
+
+console.log(array)
+
+const itemRemovido = array.pop() // o pop retorna o elemento removido
+
+console.log(itemRemovido)
+
+console.log(array)
+
+array.push("f", "d", "s")
+
+console.log(array)
+
+// 12 - shift e unshift
+
+const letters = ["a", "b", "c"]
+
+const letter = letters.shift()
+
+console.log(letter)
+
+console.log(letters)
+
+letters.unshift("p", "q", "r")
+
+letters.unshift("z")
+
+console.log(letters)
+
+// 13 - indexOf e lastIndexOf
+const myElements = ["Morango", "Maçã", "Abacate", "Pêra", "Abacate"]
+
+console.log(myElements.indexOf("Maçã"))
+console.log(myElements.indexOf("Abacate"))
+
+console.log(myElements[2])
+console.log(myElements[myElements.indexOf("Abacate")])
+
+console.log(myElements.lastIndexOf("Abacate"))
+
+console.log(myElements.indexOf("Mamão"))
+
+console.log(myElements.lastIndexOf("Mamão"))
+
+// 14 - slice
+const testeSlice =["a", "b", "c", "d", "e", "f"]
+
+const subArray = testeSlice.slice(1, 4)
+
+console.log(testeSlice)
+console.log(subArray)
+
+const subArray3 = testeSlice.slice(10, 20)
+
+console.log(subArray3)
+
+const subArray4 = testeSlice.slice(2) // vai mostrar a partir do índex 2
+
+console.log(subArray4)
+
+// 15 - forEach
+const nums = [1, 2, 3, 4, 5]
+
+nums.forEach((numero) => {
+    console.log(`O número é ${numero}`)
+})
+
+const posts = [
+    {title: "Primeiro post", category: "PHP"},
+    {title: "Segundo post", category: "JavaScript"},
+    {title: "Terceiro post", category: "Python"}
+]
+
+posts.forEach((post) => {
+    console.log(`Exibindo post: ${post.title}, da categoria: ${post.category}`)
+})
