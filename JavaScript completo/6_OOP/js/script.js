da1bf8d48ecaa6f13e241b492586d4f6d011ee23
Yarlley Fernandes <147113204+Yarlley31@users.noreply.github.com>
2023-12-07 20:59:18 -0300

    Update script.js

--- JavaScript completo/6_OOP/js/script.js
+++ JavaScript completo/6_OOP/js/script.js
@@ -57,3 +57,152 @@ console.log(mySecondObject)
 console.log(mySecondObject.a)
 
 console.log(Object.getPrototypeOf(mySecondObject) === Object.myObject)
+
+// 5 - classes básicas
+const cachorro = {
+    raca: null,
+    patas: 4
+}
+
+const pastorAlemao = Object.create(cachorro)
+
+pastorAlemao.raca = "Pastor Alemão"
+
+console.log(pastorAlemao)
+
+console.log(pastorAlemao.patas)
+
+const bulldog = Object.create(cachorro)
+
+bulldog.raca = "Bulldog"
+
+console.log(bulldog)
+
+console.log(bulldog, pastorAlemao)
+
+// 6 - função como classe - função contrutora
+function criarCachorro(nome, raca) {
+    const cachorro = Object.create({})
+
+    cachorro.nome = nome
+    cachorro.raca = raca
+
+    return cachorro
+}
+
+const bob = criarCachorro("bob", "Bulldog")
+
+console.log(bob)
+
+const pluffe = criarCachorro("Pluffe", "Poddle")
+
+console.log(pluffe)
+
+console.log(Object.getPrototypeOf(pluffe))
+
+// 7 - função como classe
+function Cachorro(nome, raca) {
+    this.nome = nome
+    this.raca = raca
+}
+
+const husky = new Cachorro("Ozzy", "Husky")
+
+console.log(husky)
+
+// 8 - métodos nas funções contrutoras
+Cachorro.prototype.uivar = function() {
+    console.log("Auuuu!")
+}
+
+console.log(Cachorro.prototype)
+
+husky.uivar()
+
+// 9 - classes ES6
+class CachorroClasse {
+    constructor(nome, raca) {
+        this.nome = nome
+        this.raca = raca
+    }
+}
+
+const jeff = new CachorroClasse("Jeff", "Labrador")
+
+console.log(jeff)
+
+console.log(Object.getPrototypeOf(jeff))
+
+//  10 - mais sobre classes
+class Caminhao {
+    constructor(eixos, cor) {
+        this.eixos = eixos
+        this.cor = cor
+    }
+
+    descreverCaminhao() {
+        console.log(`Este caminhão tem ${this.eixos} eixos e é da cor ${this.cor}`)
+    }
+}
+
+const scania = new Caminhao(6, "Vermelho")
+
+console.log(scania)
+
+scania.descreverCaminhao()
+
+Caminhao.motor = 4
+
+const c2 = new Caminhao(4, "Azul")
+
+console.log(c2)
+
+console.log(c2.motor)
+
+Caminhao.prototype.motor = 4.0
+
+const c3 = new Caminhao(4, "Dourado")
+
+console.log(c3.motor)
+
+// 11 - override
+class Humano {
+    constructor(nome, idade) {
+        this.nome = nome
+        this.idade = idade
+    }
+}
+
+const Yarlley = new Humano("Yarlley", 16)
+
+console.log(Yarlley)
+
+console.log(Humano.prototype.idade) // dará como undefined pq esta antes do prototype
+
+Humano.prototype.idade = "Não definida"
+
+console.log(Yarlley.idade)
+
+console.log(Humano.prototype.idade)
+
+// 12 - symbol
+class Aviao {
+    constructor(marca, turbina) {
+        this.marca = marca
+        this.turbina = turbina
+    }
+}
+
+const asas = Symbol()
+const pilotos = Symbol()
+
+Aviao.prototype[asas] = 2
+Aviao.prototype[pilotos] = 3
+
+const boeing = new Aviao("Boeing", 4)
+
+console.log(boeing)
+
+console.log(boeing[asas])
+
+console.log(boeing[pilotos])
