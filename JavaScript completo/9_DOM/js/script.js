53e083379802cc33580b7d17add6f47befc396d2
Yarlley Fernandes <147113204+Yarlley31@users.noreply.github.com>
2023-12-15 17:16:20 -0300

    Update script.js

--- JavaScript completo/9_DOM/js/script.js
+++ JavaScript completo/9_DOM/js/script.js
@@ -1,2 +1,80 @@
 // 1 - movendo-se pelo DOM
 console.log(document.body)
+
+console.log(document.body.childNodes[1])
+
+console.log(document.body.childNodes[1].childNodes)
+
+console.log(document.body.childNodes[1].childNodes[1].textContent)
+
+// 2 - selecionando elementos por tag
+const listItens = document.getElementsByTagName("li")
+
+console.log(listItens)
+
+// 3 - selecionando elementos por id
+const title = document.getElementById("title")
+
+console.log(title)
+
+// 4 - selecionando elementos por class
+const products = document.getElementsByClassName("products")
+
+console.log(products)
+
+// 5 - selecionando elementos por CSS
+const productsQuery = document.querySelectorAll(".products")
+
+console.log(productsQuery)
+
+const maincontainer = document.querySelector("#main-container")
+
+console.log(maincontainer)
+
+// 6 - isertBefore
+const p = document.createElement("p")
+
+console.log(p)
+
+const header = title.parentElement
+
+header.insertBefore(p, title)
+
+// 7 - appendChild
+const navLinks = document.querySelector("nav ul")
+
+const li = document.createElement("li")
+
+navLinks.appendChild(li)
+
+// 8 - replaceChild
+
+const h2 = document.createElement("h2")
+
+h2.textContent = "Novo título galerinha"
+
+header.replaceChild(h2, title)
+
+// 9 - createTextNode
+const myText = document.createTextNode("Vamos adicionar mais um título")
+
+console.log(myText)
+
+const h3 = document.createElement("h3")
+
+h3.appendChild(myText)
+
+console.log(h3)
+
+maincontainer.appendChild(h3)
+
+// 10 - trabalhando com atributos
+const firstLink = navLinks.querySelector("a")
+
+console.log(firstLink)
+
+firstLink.setAttribute("href", "https://www.google.com")
+
+console.log(firstLink.getAttribute("href"))
+
+firstLink.setAttribute("target", "_blank")
