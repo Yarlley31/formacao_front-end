6001f9b38930c2d267455440ccb8970d05592044
Yarlley Fernandes <147113204+Yarlley31@users.noreply.github.com>
2023-12-16 14:35:59 -0300

    Update script.js

--- JavaScript completo/9_DOM/js/script.js
+++ JavaScript completo/9_DOM/js/script.js
@@ -78,3 +78,17 @@ firstLink.setAttribute("href", "https://www.google.com")
 console.log(firstLink.getAttribute("href"))
 
 firstLink.setAttribute("target", "_blank")
+
+// 11 - altura e largura
+const footer =  document.querySelector("footer")
+
+console.log(footer.offsetWidth)
+console.log(footer.offsetHeight)
+
+console.log(footer.clientWidth)
+console.log(footer.clientHeight)
+
+// 12 - posição do elemento
+const products1 = products[0]
+
+console.log(products1.getBoundingClientRect())
