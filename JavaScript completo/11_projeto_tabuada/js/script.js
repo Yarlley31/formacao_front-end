4dd9a461d76727af1592fff418e1b576aff6a4d0
Yarlley Fernandes <147113204+Yarlley31@users.noreply.github.com>
2023-12-21 15:59:38 -0300

    Concluido

--- JavaScript completo/11_projeto_tabuada/js/script.js
+++ JavaScript completo/11_projeto_tabuada/js/script.js
@@ -1,1 +1,42 @@
-console.log("testet")
+// Seleção dos elementos
+const multiplicationForm = document.querySelector("#multiplication-form")
+const numberInput = document.querySelector("#number")
+const multiplicatorInput = document.querySelector("#multiplicator")
+
+const multiplicationTable = document.querySelector("#multiplication-operations")
+
+const multiplicationTitle = document.querySelector("#multiplication-title span")
+// Funções
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationTable.innerHTML = ""
+
+    for(i = 1; i <= multiplicatorNumber; i++) {
+        const result = number * i
+
+        const template = `   
+            <div class="row">
+                <div class="operation">${number} X ${multiplicatorNumber} = </div>
+                <div class="result">${result}</div>
+            </div>`
+        
+        const parser = new DOMParser()
+        const hmtlTemplate = parser.parseFromString(template, "text/html")
+
+        const row = hmtlTemplate.querySelector(".row")
+
+        multiplicationTable.appendChild(row)
+    }
+
+    multiplicationTitle.innerText = number
+}
+
+// Eventos
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const multiplicationNumber = +numberInput.value
+    const multiplicatorNumber = +multiplicatorInput.value
+
+    if(!multiplicationNumber || !multiplicatorNumber) return
+
+    createTable(multiplicationNumber, multiplicatorNumber)
+})
