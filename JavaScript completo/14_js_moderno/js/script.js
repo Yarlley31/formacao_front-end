0fbe1ca384472b090ab7aa315d7636bb7f974a03
Yarlley Fernandes <147113204+Yarlley31@users.noreply.github.com>
2024-07-05 20:53:39 -0300

    Update script.js

--- JavaScript completo/14_js_moderno/js/script.js
+++ JavaScript completo/14_js_moderno/js/script.js
@@ -28,3 +28,44 @@ const name = "Matheus"
 
 logName()
 console.log(name)
+
+// 2 - arrow functions
+const sum = function(a, b) {
+    return a + b
+}
+
+const arrowSum = (a, b) => a + b;
+
+console.log(sum(5, 5))
+
+console.log(arrowSum(5,5))
+
+const greeting = (names) => {
+    if(names) {
+        return `Olá ${names}!`
+    } else {
+        return 'Olá!'
+    }
+}
+
+console.log(greeting('Yarlley'))
+console.log(greeting())
+
+const user = {
+    name: 'Yarlley',
+    sayUsername() {
+        setTimeout(function() {
+            console.log(this)
+            console.log(`Username: ${this.name}`)
+        }, 1000)
+    },
+    sayUsernameArrow() {
+        setTimeout(() => {
+            console.log(this)
+            console.log(`Username: ${this.name}`)
+        }, 2000);
+    }
+}
+
+user.sayUsername()
+user.sayUsernameArrow()
