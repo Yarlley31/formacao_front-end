34ff6079ffeb5d34182d2975c661e56bb866b1ff
Yarlley Fernandes <147113204+Yarlley31@users.noreply.github.com>
2024-09-18 12:50:23 -0300

    inserindo dados na tela

--- JavaScript completo/12_axios/js/script.js
+++ JavaScript completo/12_axios/js/script.js
@@ -6,11 +6,40 @@ const getData = async() => {
     try {
         const response = await axios.get(
             "https://jsonplaceholder.typicode.com/users")
-        console.log(response)
-        return response.data
+        console.log(response);
+        return response.data;
     } catch(error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
-getData()
+getData();
+
+// 3 inserindo dados na tela
+const container = document.querySelector("#user-container");
+
+const printData = async () => {
+    const data = await getData();
+    
+    console.log(data);
+
+    data.forEach((user) => {
+        const div = document.createElement("div");
+
+        const nameElement = document.createElement("h2");
+
+        nameElement.textContent = user.name;
+        
+        div.appendChild(nameElement);
+
+        const emailElement = document.createElement("p");
+
+        emailElement.textContent = user.email;
+
+        div.appendChild(emailElement);
+
+        container.appendChild(div);
+    });
+};
+
+printData();
